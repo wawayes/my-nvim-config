7891d5de19f93e80cda7526c4ed3ad4a76ca5ea8
wangmingyao <wangmingyaoyes@gmail.com>
2023-11-12 16:03:09 +0800

    add share高亮插件, conflict-marker

--- init.lua
+++ init.lua
@@ -42,6 +42,29 @@ end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	{
+		"tpope/vim-rhubarb",
+		event = "VeryLazy",
+	},
+	{
+		"rhysd/conflict-marker.vim",
+		event = "VeryLazy",
+		config = function()
+			vim.cmd([[
+			let g:conflict_marker_highlight_group = ''
+
+			" Include text after begin and end markers
+			let g:conflict_marker_begin = '^<<<<<<< .*$'
+			let g:conflict_marker_end   = '^>>>>>>> .*$'
+
+			highlight ConflictMarkerBegin guibg=#2f7366
+			highlight ConflictMarkerOurs guibg=#2e5049
+			highlight ConflictMarkerTheirs guibg=#344f69
+			highlight ConflictMarkerEnd guibg=#2f628e
+			highlight ConflictMarkerCommonAncestorsHunk guibg=#754a81
+			]])
+		end,
+	},
+	{
 		event = "VeryLazy",
 		"hrsh7th/nvim-cmp",
 		dependencies = {
